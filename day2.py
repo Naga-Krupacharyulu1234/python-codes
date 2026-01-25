758411bab8e6bd22ea046fe5805a433115696567
Naga krupacharyulu <nagakrupacharyulu516@gmail.com>
2026-01-25 21:17:43 +0530

    Implement sum and difference calculations
    Added arithmetic operations for sum and difference.

---
+++ day2.py
@@ -0,0 +1,15 @@
+# python-codes assign a integer value to enter no of times
+!.number  = int(input())  #which resemmbles the input is an integer and that can be enter no of times
+print(num)
+output is whwtever you enter the input it is the output
+2.#multiplication or the square
+n=int(input())
+print(n*n)
+3.#arthemetic operation in seperate line
+a = int(inpu())
+b = int(input())
+sum = a+b
+diff = a-b
+print(sum)
+print(diff)
+
