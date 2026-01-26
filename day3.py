60338dfe6eaf01ce687ec06660c3ecaed97ed2f1
Naga krupacharyulu <nagakrupacharyulu516@gmail.com>
2026-01-26 12:53:18 +0530

    Implement voting eligibility check based on age
    Add a simple age check for voting eligibility.

---
+++ day3.py
@@ -0,0 +1,8 @@
+#conditional statements
+#eligible for vote or not eligible
+age = int(input("Emter your age:"))
+if age >= 18:
+  print("Eligible for to vote")
+else:
+  print("Not eligible")
+  print("not enough age to vote)
