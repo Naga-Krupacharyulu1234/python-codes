29863cde5134933062c879f7478c0d607fa6817c
Naga krupacharyulu <nagakrupacharyulu516@gmail.com>
2026-01-27 21:28:01 +0530

    Add conditional statements for age and grade

---
+++ day4.py
@@ -0,0 +1,13 @@
+#conditional statement
+1. age = int(input())
+if age>=20:
+print("coding is fun")
+else:
+print("is not fun")
+#elif
+grade = 85
+
+if grade >= 90:
+    print("You got an A")
+elif grade >= 80:
+    print("You got a B")
