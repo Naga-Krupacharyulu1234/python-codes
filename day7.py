d22d0d00cf5d25913dd17d5fa4dfb43359a28ae4
Naga krupacharyulu <nagakrupacharyulu516@gmail.com>
2026-08-01 21:10:14 +0530

    Add linear search and max/min element functions
    Implement linear search, find maximum and minimum elements in arrays.

---
+++ day7.py
@@ -0,0 +1,26 @@
+linear search
+arr=[1,2,3,4,5]
+target=4
+found=false
+for i in range(len(arr)):
+  if arr[i]==target:
+    print("element found at :",i)
+    found=true
+    break
+    if not found:
+      print("element not found")
+MAXIMUM ELEMENTS
+arr=[12,13,14,15,16]
+maximum=arr[0]
+for num in arr:
+  if num>maximum:
+    print("maximum",maximum)
+MINIMUM ELEMENTS
+arr=[1,22,333,44,55,66]
+minimum=arr[0]
+for num in arr:
+  if num<mainimum:
+    minimum=num
+  print("minimum:",minimum)
+  
+    
